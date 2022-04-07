235291880d58168baa3af0573050245da6332582
yukiny0811 <yukiny0811@gmail.com>
2022-04-07 10:33:04 +0900

    Initial Commit

---
+++ MetalTemplate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MetalTemplate
+//
+//  Created by クワシマ・ユウキ on 2022/04/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
